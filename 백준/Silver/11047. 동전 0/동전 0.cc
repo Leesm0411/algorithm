898d4f81050885dae4a43cee132ea17489fae731
Leesm0411 <lesemin0411@gmail.com>
2026-04-17 11:41:36 +0900

    [Silver IV] Title: 동전 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N, K;
+    cin >> N >> K;
+    vector<int> arr(N);
+    for(int i = 0; i < N; i++){
+        cin >> arr[i];
+    }
+    int cnt = 0;
+    for(int i = N - 1; i >= 0; i--){
+        cnt += K / arr[i];
+        K %= arr[i];
+    }
+    cout << cnt;
+    
+    return 0;
+}
